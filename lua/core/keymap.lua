94c0ec2db7b27f497b9f6359f3e3e0279e3ecdd3
Frode Ulriksen <frode.ulriksen@outlook.com>
2024-08-22 21:52:33 +0200

    New setup

---
+++ lua/core/keymap.lua
@@ -0,0 +1,87 @@
+vim.keymap.set('i', 'kj', '<Esc>', { desc = "Use 'kj' keys in quick succession for ESC" })
+
+-- remap pane navigation
+vim.keymap.set('n', '<C-j>', '<CMD>NavigatorDown<CR>', { desc = "Move to down pane" })
+vim.keymap.set('n', '<C-k>', '<CMD>NavigatorUp<CR>', { desc = "Move to up pane" })
+vim.keymap.set('n', '<C-h>', '<CMD>NavigatorLeft<CR>', { desc = "Move to left pane" })
+vim.keymap.set('n', '<C-l>', '<CMD>NavigatorRight<CR>', { desc = "Move to right pane" })
+
+-- Paste without overwriting register
+vim.keymap.set("v", "p", '"_dP', { desc = "Paste without overwriting register"})
+
+-- Copy text to " register
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y", { desc = "Yank into \" register" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Yank into \" register" })
+
+-- Delete text to " register
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete into \" register" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete into \" register" })
+
+-- Move lines up or down when in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep window centered when going up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<space><space>", "<cmd> :noh<CR>", { desc = "Clear search highlighting" })
+
+vim.keymap.set("n", "<leader>t", function()
+   vim.cmd("cd %:p:h")
+   vim.cmd("belowright split | terminal")
+   vim.cmd("startinsert")
+end, { desc = "Open terminal in folder of current file"})
+
+-- Find and replace visual selection using CTRL-r
+vim.keymap.set("v", "<C-r>", "\"hy:%s/<C-r>h//gc<left><left><left>")
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<Leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
+
+-- Toggle terminal mappings
+vim.api.nvim_set_keymap('n', '<Leader>tt', ':ToggleTerm<CR>', { noremap = true, silent = true })
+function _G.set_terminal_keymaps()
+    local opts2 = { buffer = 0 }
+    vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], opts2)
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts2)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts2)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts2)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts2)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts2)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts2)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- FineCmd
+vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
+
---
+++ lua/core/plugins.lua
@@ -0,0 +1,122 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  'ellisonleao/gruvbox.nvim',
+  'nvim-tree/nvim-tree.lua',
+  'nvim-tree/nvim-web-devicons',
+  'nvim-lualine/lualine.nvim',
+  'nvim-treesitter/nvim-treesitter',
+  'kylechui/nvim-surround',
+  'windwp/nvim-autopairs',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-buffer',
+  'lewis6991/gitsigns.nvim',
+  'rose-pine/neovim',
+  'tomasiser/vim-code-dark',
+  'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
+  'preservim/nerdcommenter',
+  'whiteinge/diffconflicts',
+  'simrat39/rust-tools.nvim',
+  'nvim-lua/plenary.nvim',
+  'lambdalisue/suda.vim',
+  'numToStr/Navigator.nvim',
+  'jakewvincent/mkdnflow.nvim',
+  'catppuccin/nvim',
+  'echasnovski/mini.icons',
+  'romgrk/barbar.nvim',
+  'kdheepak/lazygit.nvim',
+  'MunifTanjim/nui.nvim',
+
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    --version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
+
+  { 'VonHeikemen/fine-cmdline.nvim',
+        requires = {
+            {
+                'MunifTanjim/nui.nvim'
+            }
+        }
+  },
+
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+
+  {
+    'nvim-telescope/telescope.nvim', version = '0.1.4',
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+
+  {
+    "AckslD/nvim-neoclip.lua",
+    dependencies = {
+      {'nvim-telescope/telescope.nvim'},
+    }
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  { 'folke/which-key.nvim',                   -- Keybindings
+     config = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        require("which-key").setup {
+            auto = true,
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+     end
+  },
+  { 'akinsho/toggleterm.nvim',
+      config = function()
+        require("toggleterm").setup({
+        close_on_exit = false,
+        autochdir = true,
+        shell = vim.o.shell,
+        size = 20
+      })
+    end
+  },
+}
+
+require("lazy").setup(plugins, opts)
